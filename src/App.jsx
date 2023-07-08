7d45b6e01b4868b31762563b4fc5dba2e235d1d6
root <root@DESKTOP-RAKF29J>
2023-07-08 15:30:42 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // step 1 import React
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Main from './components/main'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
