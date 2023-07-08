f6f68c5d8523cc584a5c91998865dc1ac52b7e27
root <root@DESKTOP-RAKF29J>
2023-07-08 15:07:12 -0500

    added main to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main from ''
+import Main from './components/main'
 
 
 // step2 create a component
