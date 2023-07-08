bb150ce2302ef085f69b14c445ac95c85b41d75b
root <root@DESKTOP-RAKF29J>
2023-07-08 15:44:01 -0500

    add click

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -4,10 +4,26 @@ import React from 'react';
 
 // create component
 class HornedBeast extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            LikesCount: 0
+        };
+    }
+
+    handleLikes = () => {
+        this.setState({
+            LikesCount: this.state.LikesCount + 1
+        });
+    };
+
     render() {
         return (
             <><div>
                 <h2>{this.props.title}</h2>
+                <p onClick={this.handleLikes}></p>
+                <p>Likes: {this.state.Likes}</p>
                 <img src={this.props.imgLink} />
                 <p>{this.props.description}</p>
             </div></>
