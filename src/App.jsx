dc9cd6f4b32ef9b0e8bcd16bb640a023d868279b
root <root@DESKTOP-RAKF29J>
2023-07-08 15:25:55 -0500

    added export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // step 1 import React
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './Header';
+import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -22,3 +22,4 @@ class Main extends React.Component {
     )
   }
 }
+export default Main;
