6b8262bf9115c094c996e6365a4327646191da91
root <root@DESKTOP-RAKF29J>
2023-07-08 15:06:18 -0500

    moved main

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import HornedBeast from './components/HornedBeast.jsx'
-import App from './App.jsx'
-import data from './components/data.json'
+import HornedBeast from './HornedBeast.jsx'
+import App from '../App.jsx'
+import data from './data.json'
 import './index.css'
 
 class Main extends React.Component {
 
   render() {
     let beastComponents = [];
-    data.forEach((title) => {
+    data.forEach((beast) => {
       beastComponents.push(
         <HornedBeast title={beast.title} imgLink={beast.image_url} />
       )
