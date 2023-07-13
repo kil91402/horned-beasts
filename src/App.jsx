768931bbd2bcb2b2d82669b0f6e0398115d203a1
root <root@DESKTOP-RAKF29J>
2023-07-12 21:56:01 -0500

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import data from './data.json'
+import data from './components/data.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
