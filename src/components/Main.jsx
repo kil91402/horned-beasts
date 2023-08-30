9ce84d4ab2cbe7250d686eebed61bc6fcb8ae2e8
Ajamu Page <kil91402@gmail.com>
2023-08-29 19:18:00 -0500

    added for each

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,6 +11,20 @@ class Main extends React.Component {
   render() {
     console.log(this.props.filteredBy);
     let beastComponents = [];
+    this.props.beastData.forEach((beast) => {
+      beastComponents.push(
+        <Col key={beast._id} xs={3}>
+          <HornedBeast
+            title={beast.title}
+            description={beast.description}
+            ImgLink={beast.image_url}
+            handleShowModal={this.props.handleShowModal}
+            beast={beast}
+          />
+        </Col>
+      );
+    }
+  );
     
     this.props.beastData.forEach((beast) => {
       beastComponents.push(
